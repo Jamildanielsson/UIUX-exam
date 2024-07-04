214cde7c615d5bbcf3407c264001891a9e4f4abd
Jamildanielsson <114480495+Jamildanielsson@users.noreply.github.com>
2024-07-04 18:01:18 +0200

    feat: updated paths

--- src/views/ToFly/ToFly.jsx
+++ src/views/ToFly/ToFly.jsx
@@ -10,7 +10,7 @@ function ToFly() {
       <Header />
       <section className='kids__section'>
         <h1 tabIndex="0" className='tofly__heading-lv1'>INFÖR DIN RESA</h1>
-        <img className='tofly__img' src="../../src/assets/cryingbaby.png" alt="Ett barn som befinner sig på ett flygplan och skriker rakt ut" /><br></br><span tabIndex='0' className='margin-extra'>  En bebis gråter på flygplanet</span><br></br>
+        <img className='tofly__img' src="/src/assets/cryingbaby.png" alt="Ett barn som befinner sig på ett flygplan och skriker rakt ut" /><br></br><span tabIndex='0' className='margin-extra'>  En bebis gråter på flygplanet</span><br></br>
         <h2 tabIndex="0" className='tofly__heading-lv2'>Att flyga med barn</h2>
         <p tabIndex="0" className='tofly__paragraph'>Att flyga med barn kan vara svårt ibland, dem kan skrika när de känner obehag och att sitta fast
           med bälte kan skapa en klaustrofobisk känsla vilket enkelt kan stressa ett litet barn.
@@ -18,7 +18,7 @@ function ToFly() {
           kunna förbereda båda er vuxna men framförallt barnen inför flygresan.
           Vi hoppas att vår video skall kunna hjälpa er med er nästa flygresa.</p>
         <video tabIndex="0" className='tofly__video' controls >
-          <source name='Instruktionsvideo' src="../../src/assets/video/video.mp4" type="video/mp4" />
+          <source name='Instruktionsvideo' src="/src/assets/video/video.mp4" type="video/mp4" />
           <track src="../../src/assets/video/video.vtt" srcLang="en" label="English" kind="captions" default={true}></track>
           Din webbläsare stödjer inte denna typ av filmfiler.
           <a href='https://www.youtube.com/watch?v=JFQU_A5YjJw'>Länk till videon istället</a>
@@ -30,7 +30,7 @@ function ToFly() {
           Behöver du hjälp med din flygrädsla? Använd länken nedan för att få råd inför resan</p>
         <a className='tofly__link' href="https://www.sveakbt.se/fobier/flygradsla">Länk: Läs mer här om flygrädsla</a>
         <br></br>
-        <img className='tofly__img' src="../../src/assets/cartoon.png" alt="En tecknad bild på en man ombord på ett flygplan som skakar av rädsla" /><br></br><span tabIndex='0' className='margin-extra'>En man skakar av rädsla i sin flygplans-stol</span><br></br><br></br>
+        <img className='tofly__img' src="/src/assets/cartoon.png" alt="En tecknad bild på en man ombord på ett flygplan som skakar av rädsla" /><br></br><span tabIndex='0' className='margin-extra'>En man skakar av rädsla i sin flygplans-stol</span><br></br><br></br>
       </section>
       <button className='main__button' onClick={() => navigate('/')}>TILLBAKA TILL START</button>
     </main>
